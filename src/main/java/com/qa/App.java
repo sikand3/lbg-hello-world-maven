4ccb5dd8e6dcd9aff79e044b7dc5f9043ceafb93
unknown <Sorabh Sikand>
2024-04-16 14:37:48 +0100

    name added

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,7 +13,7 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
-        System.out.println(sayHelloToSomeone("Sorabh"))
+        System.out.println(sayHelloToSomeone("Sorabh"));
         System.out.println(sayGoodbye());
     }
 
